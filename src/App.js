4463a6d38c517e41e393dc566b144c8358e2097a
suvarnasomvanshi <119663386+suvarnasomvanshi@users.noreply.github.com>
2023-09-29 16:34:49 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {addTodo,deleteTodo,removeAllTodo} from "./components/Actions";
+import "./App.css";
+
 
 
 
@@ -12,20 +14,22 @@ const dispatch = useDispatch()
 console.log(todoListArr)
   return(
     <>
-        <h1>todo List</h1>
-
+       <div className="container">
+        <h1> Todo List </h1>
+        <div className="container1">
         <input 
         type="text"
         value={inputData}
         placeholder ="add todos"
         onChange={(e)=>setInputData(e.target.value)}
+        className="_add"
         />
         <button onClick={()=>dispatch(addTodo(inputData),setInputData(""))}>add todo</button>
 
         {todoListArr.map((ele)=>{
           return(
             <>
-            <div key={ele.id}>
+            <div key={ele.id} className="_Add">
                    <li>
                    {ele.data}
                    <button onClick={()=>dispatch(deleteTodo(ele.id))}>delete todo</button>
@@ -35,7 +39,14 @@ console.log(todoListArr)
           )
         })}
 
-        <button onClick={()=>dispatch(removeAllTodo())}>remove all todos</button>
+        </div>
+        
+        <div className="container2">
+        <button onClick={()=>dispatch(removeAllTodo())} className="_RemoveAll">remove all todos</button>
+        </div>
+
+
+        </div>
     </>
   )
 }
